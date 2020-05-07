1135eff50c6382fd1046cc55bcbfc4d87f37f03d
ben-deverell <20Danielle>
2020-05-07 13:14:39 +0100

    added linked table to category page

--- admin/categories.php
+++ admin/categories.php
@@ -1,9 +1,9 @@
-<?php include "includes/header.php"; ?>
+<?php include "includes/admin_header.php"; ?>
 
 <div id="wrapper">
 
     <!-- Navigation -->
-    <?php include "includes/navigation.php"; ?>
+    <?php include "includes/admin_navigation.php"; ?>
 
 
 
@@ -36,6 +36,13 @@
                     </div>
                     <div class="col-xs-6">
 
+                        <?php
+                        $query = "SELECT * FROM categories";
+                        $select_categories = mysqli_query($connection, $query);
+                        ?>
+
+
+
                         <table class="table table-bordered table-hover table-hover">
                             <thead>
                                 <tr>
@@ -44,11 +51,18 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>Baseball Category</td>
-                                    <td>Basketball Category</td>
-                                </tr>
 
+                                <?php
+                                while ($row = mysqli_fetch_array($select_categories)) {
+                                    $cat_id =  $row['cat_id'];
+                                    $cat_title =  $row['cat_title'];
+
+                                    echo "<tr>";
+                                    echo "<td>{$cat_id}</td>";
+                                    echo "<td>{$cat_title}</td>";
+                                    echo "<tr>";
+                                }
+                                ?>
                             </tbody>
                         </table>
 
@@ -65,4 +79,4 @@
     </div>
     <!-- /#page-wrapper -->
 
-    <?php include "includes/footer.php"; ?>
+    <?php include "includes/admin_footer.php"; ?>
