74df1e9e41d51875d1c064a46dc25d3fdc25cc4f
ben-deverell <20Danielle>
2020-05-11 10:43:53 +0100

    refactored php moved to functions.php

--- admin/categories.php
+++ admin/categories.php
@@ -25,23 +25,7 @@
                     <div class="col-xs-6">
 
                         <?php
-                        if (isset($_POST['submit'])) {
-
-
-                            $cat_title = $_POST['cat_title'];
-                            if ($cat_title == "" || empty($cat_title)) {
-                                echo "Category field is empty";
-                            } else {
-                                $query = "INSERT INTO categories(cat_title) ";
-                                $query .= "VALUES('{$cat_title}') ";
-
-                                $create_category_query = mysqli_query($connection, $query);
-
-                                if (!$create_category_query) {
-                                    die('QUERY FAILED' . mysqli_error($connection));
-                                }
-                            }
-                        }
+                        insert_categories();
                         ?>
 
                         <form action="" method="post">
