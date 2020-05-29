29354eebe7d261fc2c96eab0420d6531c46b11cd
ben-deverell <20Danielle>
2020-05-29 18:09:43 +0100

    fixed edit_post functionality

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -4,7 +4,7 @@ if (isset($_GET['p_id'])) {
 
     $the_post_id = $_GET['p_id'];
 }
-$query = "SELECT * FROM posts";
+$query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
 $select_posts_by_id = mysqli_query($connection, $query);
 
 while ($row = mysqli_fetch_array($select_posts_by_id)) {
@@ -19,6 +19,48 @@ while ($row = mysqli_fetch_array($select_posts_by_id)) {
     $post_date =  $row['post_date'];
 }
 
+if (isset($_POST['update_post'])) {
+
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_comment = $_POST['post_comment'];
+    $post_tags = $_POST['post_tags'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if (empty($post_image)) {
+
+        $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+        $select_image = mysqli_query($connection, $query);
+
+        while ($row = mysqli_fetch_array($select_image)) {
+
+            $post_image = $row['post_image'];
+        }
+    }
+
+    $post_title = mysqli_real_escape_string($connection, $post_title);
+
+
+    $query = "UPDATE posts SET ";
+    $query .= "post_title  = '{$post_title}', ";
+    $query .= "post_category_id = '{$post_category_id}', ";
+    $query .= "post_date   =  now(), ";
+    $query .= "post_author = '{$post_author}', ";
+    $query .= "post_status = '{$post_status}', ";
+    $query .= "post_tags   = '{$post_tags}', ";
+    $query .= "post_comment= '{$post_comment}', ";
+    $query .= "post_image  = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_post_id} ";
+
+    $update_post = mysqli_query($connection, $query);
+    confirmQuery($update_post);
+}
+
 ?>
 
 
@@ -26,19 +68,19 @@ while ($row = mysqli_fetch_array($select_posts_by_id)) {
 <form action="" method="post" enctype="multipart/form-data">
 
     <div class="form-group">
-        <label for="title">Post Title</label>
-        <input value="<?php echo $post_title;  ?>" type="text" class="form-control" name="title">
+        <label for="post_title">Post Title</label>
+        <input value="<?php echo $post_title;  ?>" type="text" class="form-control" name="post_title">
     </div>
 
     <div class="form-group">
-        <select name="" id="">
+        <select name="post_category_id" id="">
 
             <?php
 
             $query = "SELECT * FROM categories";
             $select_categories = mysqli_query($connection, $query);
 
-            confirm($select_categories);
+            confirmQuery($select_categories);
 
             while ($row = mysqli_fetch_array($select_categories)) {
                 $cat_id =  $row['cat_id'];
@@ -51,8 +93,9 @@ while ($row = mysqli_fetch_array($select_posts_by_id)) {
 
     </div>
 
-    <div class="form-group"><label for="author">Post Author</label>
-        <input value="<?php echo $post_author;  ?>" type="text" class="form-control" name="author" id="author">
+    <div class="form-group"><label for="post_author">Post Author</label>
+        <input value="<?php echo $post_author;  ?>" type="text" class="form-control" name="post_author"
+            id="post_author">
     </div>
 
     <div class="form-group"><label for="post_status">Post Status</label>
@@ -60,8 +103,10 @@ while ($row = mysqli_fetch_array($select_posts_by_id)) {
     </div>
 
 
-    <div class="post-group">
-        <img width="100" src="../images/<?php echo $post_image;  ?>" alt="">
+    <div class="form-group">
+
+        <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
+        <input type="file" name="image">
     </div>
 
     <div class="form-group"><label for="post_tags">Post Tags</label>
@@ -70,13 +115,13 @@ while ($row = mysqli_fetch_array($select_posts_by_id)) {
 
     <div class="form-group">
         <label for="post_comment">Post Content</label>
-        <textarea class="form-control" name="post_comment" id="" cols="30" rows="10"><?php echo $post_comment;  ?>   
+        <textarea class="form-control" name="post_comment" id="" cols="30" rows="10"><?php echo $post_comment; ?>   
     </textarea>
 
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+        <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
     </div>
 
 </form>
