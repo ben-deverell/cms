3cc0099a4bd7c853a15b0f499cddec6b746a74e5
ben-deverell <20Danielle>
2020-06-08 14:30:14 +0100

    modified view_all_comments completing crud view

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -33,7 +33,6 @@
             echo "<tr>";
             echo "<td>$comment_id</td>";
             echo "<td>$comment_author</td>";
-            echo "<td>$comment_email</td>";
             echo "<td>$comment_content</td>";
 
 
@@ -48,16 +47,14 @@
             //     echo "<td>{$cat_title}</td>";
             // }
 
-
+            echo "<td>$comment_email</td>";
             echo "<td>$comment_status</td>";
+            echo "<td>Some Title</td>";
             echo "<td>$comment_date</td>";
 
-            echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Approve</a></td>";
-            echo "<td><a href='posts.php?delete={$post_id}'>Unapprove</a></td>";
-            echo "</tr>";
-
-            echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-            echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
+            echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
+            echo "<td><a href='posts.php?delete='>Unapprove</a></td>";
+            echo "<td><a href='posts.php?delete='>Delete</a></td>";
             echo "</tr>";
         }
         ?>
