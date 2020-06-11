1365079f2a07b7c691921420ade8f4756f7500b1
ben-deverell <20Danielle>
2020-06-11 09:59:43 +0100

    added users files add, edit view_all and made table in mysql

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -42,7 +42,7 @@
                         <a href="./posts.php">View All Posts</a>
                     </li>
                     <li>
-                        <a href="posts.php?source=add_post"=>Add posts</a>
+                        <a href="posts.php?source=add_post">Add posts</a>
                     </li>
                 </ul>
             </li>
@@ -57,10 +57,10 @@
                     Users <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="demo" class="collapse">
                     <li>
-                        <a href="#">Dropdown Item</a>
+                        <a href="users.php">View All Users</a>
                     </li>
                     <li>
-                        <a href="#">Dropdown Item</a>
+                        <a href="users.php?source=add_user">Add User</a>
                     </li>
                 </ul>
             </li>
